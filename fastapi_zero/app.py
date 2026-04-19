3c47fe294e8d7a41679049462017874eb63a6d57
Arthur de Souza Oliveira <arthurso93@gmail.com>
2026-04-19 10:59:26 -0300

    Adicionando schema de Message

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,8 +1,16 @@
+from http import HTTPStatus
 from fastapi import FastAPI
+
+from fastapi_zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get(
+    '/',
+    status_code=HTTPStatus.OK,
+    response_model=Message
+)
 def read_root():
+    # return 123 this will return erro because it does not match the response model
     return {'message': 'Hello World!'}
